3dd0d35cca54cb54c2058272d9e13b03cae34530
rule <ivan.jukic89@gmail.com>
2018-07-17 16:17:38 +0200

    Added core dependecies and splash screen

--- app/src/main/java/net/peercoin/peercoinwallet/SplashActivity.kt
+++ app/src/main/java/net/peercoin/peercoinwallet/SplashActivity.kt
@@ -1,9 +1,9 @@
 package net.peercoin.peercoinwallet
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
