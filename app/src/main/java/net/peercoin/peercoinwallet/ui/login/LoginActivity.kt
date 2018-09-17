31a0b54a042f054ad4900fe48e7c885292e5149b
crnmarin <crnogorac.marin@gmail.com>
2018-09-17 17:14:29 +0200

    Splash screen animation and build changes

---
+++ app/src/main/java/net/peercoin/peercoinwallet/ui/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package net.peercoin.peercoinwallet.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.peercoin.peercoinwallet.R
+
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
